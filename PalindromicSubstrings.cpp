75118c3c2eb6d670b0bd0c2f8346e25f7f2f602c
Vaibhavi Pathak <73771753+vaibhavipathakk@users.noreply.github.com>
2021-06-26 15:09:02 +0530

    Create PalindromicSubstrings.cpp

---
+++ PalindromicSubstrings.cpp
@@ -0,0 +1,57 @@
+/*
+647. Palindromic Substrings
+
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+
+
+*/
+
+
+
+class Solution {
+public:
+    int palin(string s, int l, int r){
+        int count=0;
+        while(l>=0 && r<s.length() && s[l]==s[r]){
+            count++;
+            l--;
+            r++;
+        }
+        return count;
+    }
+    
+    
+    int countSubstrings(string s) {
+        int ans=0;
+        
+        for(int i=0; i<s.length(); i++){
+            ans+=palin(s,i,i);
+            ans+=palin(s,i,i+1);
+        }
+        
+        return ans;
+    }
+};
